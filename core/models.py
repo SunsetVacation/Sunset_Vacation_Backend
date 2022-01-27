dfcfc01cdb97e0ee2189f1d06e3338e1db68b805
sumaiyaazad <azadsumaiya00@gmail.com>
2022-01-27 10:03:00 +0600

    added User model

--- core/models.py
+++ core/models.py
@@ -1,6 +1,49 @@
 from django.db import models
 
 
+class User(models.model):
+    userId = models.AutoField(
+        primary_key=True
+    )
+
+    firstName = models.CharField(
+        max_length=100,
+        allow_blank=False,
+        allow_null=False,
+        trim_whitespace=True
+    )
+
+    lastName = models.CharField(
+        max_length=100,
+        allow_blank=False,
+        allow_null=False,
+        trim_whitespace=True
+    )
+
+    email = models.EmailField(
+        max_length=50,
+        allow_blank=false,
+        allow_null=True,
+        unique=True
+    )
+
+    phoneNo = models.CharField(
+        max_length=20,
+        allow_blank=False,
+        allow_null=True,
+        trim_whitespace=True
+    )
+
+    host = models.BooleanField(
+        default=False
+    )
+
+    addressId = models.ForeignKey(
+        Address,
+        on_delete=models.SET_NULL
+    )
+
+
 class Profile(models.Model):
     id = models.AutoField(
         primary_key=True
